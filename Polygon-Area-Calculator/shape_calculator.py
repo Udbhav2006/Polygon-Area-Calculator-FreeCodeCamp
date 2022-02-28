3e955e399ade8ecbe430eb701d06ad7d5228aff3
Udbhav2006 <n.udbhav2006@gmail.com>
2022-02-28 17:15:50 +0530

    test amt inside refinement left

--- Polygon-Area-Calculator/shape_calculator.py
+++ Polygon-Area-Calculator/shape_calculator.py
@@ -19,22 +19,40 @@ class Rectangle():
     return (self.width ** 2 + self.height ** 2) ** .5
 
   def get_picture(self):
-    pic = ''
-    for i in self.height:
-      pic += '*' * self.width + '\n'
-    return pic
+    if self.height > 50  or self.width > 50:
+      return "Too big for picture."
+    else: 
+      pic = ''
+      for i in range(0, self.height):
+        pic += '*' * self.width + '\n'
+      return pic
 
   def get_amount_inside(self, shape):
     height_left = self.height
     count = 0
-    for i in shape.height:
-      if i < height_left:
-        height_left -= i
-        count += self.width // shape.width
+    while shape.height < height_left:
+      height_left -= shape.height
+      count += self.width // shape.width
     return count
   
   def __str__(self):
-    return f"Rectangle(width = {str(self.width)}, height = {str(self.height)})"
+    return f"Rectangle(width={str(self.width)}, height={str(self.height)})"
 
-class Square:
-  pass
+class Square(Rectangle):
+  def __init__(self, side):
+      self.width = int(side)
+      self.height = int(side)
+  
+  def set_side(self, new_side):
+    self.width = int(new_side)
+    self.height = int(new_side)
+
+  def set_height(self, new_height):
+    self.set_side(new_height)
+  
+  def set_width(self, new_width):
+      self.set_side(new_width)
+  
+  def __str__(self):
+      return f"Square(side={self.width})"
+
